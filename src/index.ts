a34aaf3a6c9acf06bb97f98c7ada60af4bb3139a
Kana Taguchi <kana.taguchi@relic.co.jp>
2022-10-20 20:15:15 +0900

    first and second chapter done

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+const message: string = 'Hello, World!';
+console.log(message)
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+// ./node_modules/.bin/ts-node src/index.ts
